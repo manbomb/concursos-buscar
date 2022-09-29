1b1e94acdc15a464cca2a8dc7d00c1206e70a60e
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2022-09-29 07:16:01 -0300

    toastify config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+        <ToastContainer />
         <App />
     </React.StrictMode>
 );
