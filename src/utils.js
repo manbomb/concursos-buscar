eb6c71f5ea584872e068955b1f7dbed8daf4a331
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2022-09-29 22:32:27 -0300

    create reduce unique util

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,13 @@ const parseSalario = (str = "") => {
     return parseFloat(str.replace(/[\R\$\s\.]/g, "").replace(",", "."));
 };
 
+const reduceUnique = (ac, el) => {
+    const newAc = [...ac];
+    if (!newAc.includes(el)) newAc.push(el);
+    return newAc;
+};
+
 module.exports = {
-    parseSalario
+    parseSalario,
+    reduceUnique
 };
