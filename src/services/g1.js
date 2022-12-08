64b1715124dcb86dd1ffeca7c55fd0ff70eb9e62
Sérgio Avilla <sergio.avilla@plinenergia.com.br>
2022-12-08 13:53:31 -0300

    fix and build

--- src/services/g1.js
+++ src/services/g1.js
@@ -3,13 +3,9 @@ const { default: parseConcursos } = require("../parsers/parseConcursos");
 
 const urlData = "https://s3.glbimg.com/v1/AUTH_529fe1362b534020a55de48496cc9da8/especiais/2018/lista-de-concursos/data/data.json";
 
-const getConcursos = async () => {
+export const getConcursos = async () => {
     return axios.get(urlData)
         .then((res) => res.data)
         .then((data) => data.docs)
         .then(concursos => parseConcursos(concursos));
-};
-
-module.exports = {
-    getConcursos
 };
--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,10 @@
-const parseSalario = (str = "") => {
+export const parseSalario = (str = "") => {
     if (!str) return null;
     return parseFloat(str.replace(/[\R\$\s\.]/g, "").replace(",", "."));
 };
 
-const reduceUnique = (ac, el) => {
+export const reduceUnique = (ac, el) => {
     const newAc = [...ac];
     if (!newAc.includes(el)) newAc.push(el);
     return newAc;
-};
-
-module.exports = {
-    parseSalario,
-    reduceUnique
 };
